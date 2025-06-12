af31a38f20a0656c56da4b5e0e68ad2d579dd767
Fabian280995 <lessmannwebdev@gmail.com>
2025-06-12 21:56:47 +0200

    feat: implement LinearGradient background for DiaryLayout header

--- src/app/(protected)/(tabs)/diary/_layout.tsx
+++ src/app/(protected)/(tabs)/diary/_layout.tsx
@@ -3,6 +3,7 @@ import { Stack, useRouter } from "expo-router";
 import HeaderIconButton from "@/components/screens/HeaderIconButton";
 import { useTheme } from "@/providers/theme";
 import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function DiaryLayout() {
   const router = useRouter();
@@ -13,9 +14,22 @@ export default function DiaryLayout() {
       screenOptions={{
         // iOS: transparenter Header + Blur; andere: Farbe
         headerTransparent: Platform.OS === "ios",
-        headerStyle: {
-          backgroundColor: colors.background,
-        },
+        headerBackground: () => (
+          <LinearGradient
+            colors={[
+              `${colors.background}FF`,
+              `${colors.background}FF`,
+              `${colors.foreground}00`,
+            ]}
+            style={{
+              position: "absolute",
+              inset: 0,
+            }}
+            //from bottom to top
+            start={{ x: 0, y: 0 }}
+            end={{ x: 0, y: 1 }}
+          />
+        ),
         /** wichtig, sonst liegt dein Screen unter dem Header */
         contentStyle: { backgroundColor: "transparent" },
       }}
@@ -32,6 +46,13 @@ export default function DiaryLayout() {
               iconName="settings"
               onPress={() => router.push("/settings")}
               color={colors.text}
+              style={{
+                shadowColor: colors.shadow,
+                shadowOffset: { width: 0, height: 2 },
+                shadowOpacity: 0.25,
+                shadowRadius: 3.84,
+                elevation: 3,
+              }}
             />
           ),
         }}
