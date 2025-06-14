954b4397db1c7b800d650ac1bf5dd92f046dd563
Fabian280995 <lessmannwebdev@gmail.com>
2025-06-14 19:57:04 +0200

    feat: enhance MonthSwitchHeaderBtns with animations and update current date functionality

--- src/components/calendar/MonthSwitchHeaderBtns.tsx
+++ src/components/calendar/MonthSwitchHeaderBtns.tsx
@@ -3,6 +3,11 @@ import React from "react";
 import { useTheme } from "@/providers/theme";
 import { useCalendar } from "@/providers/calendar";
 import { Feather } from "@expo/vector-icons";
+import Animated, {
+  FadeIn,
+  FadeOut,
+  LinearTransition,
+} from "react-native-reanimated";
 
 interface Props {
   foreground?: boolean;
@@ -10,28 +15,71 @@ interface Props {
 
 const MonthSwitchHeaderBtns = ({ foreground }: Props) => {
   const { colors } = useTheme();
-  const { increaseMonth, decreaseMonth } = useCalendar();
+  const { currentDate, increaseMonth, decreaseMonth, updateCurrentDate } =
+    useCalendar();
   const bgColor = foreground ? colors.foreground : colors.background;
+
+  const isNotToday = () => {
+    const today = new Date();
+    return (
+      currentDate.getDate() !== today.getDate() ||
+      currentDate.getMonth() !== today.getMonth() ||
+      currentDate.getFullYear() !== today.getFullYear()
+    );
+  };
+
   return (
     <View style={{ flexDirection: "row", gap: 8 }}>
-      <TouchableOpacity
-        style={{
-          width: 32,
-          height: 32,
-          alignItems: "center",
-          justifyContent: "center",
-          borderRadius: 100,
-          backgroundColor: bgColor,
-        }}
-        onPress={decreaseMonth}
-      >
-        <Feather
-          name="chevron-left"
-          size={24}
-          color={colors.icon}
-          style={{ marginLeft: -2 }}
-        />
-      </TouchableOpacity>
+      <Animated.View layout={LinearTransition}>
+        <TouchableOpacity
+          style={{
+            width: 32,
+            height: 32,
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 100,
+            backgroundColor: bgColor,
+          }}
+          onPress={decreaseMonth}
+        >
+          <Feather
+            name="chevron-left"
+            size={24}
+            color={colors.icon}
+            style={{ marginLeft: -2 }}
+          />
+        </TouchableOpacity>
+      </Animated.View>
+      {isNotToday() && (
+        <Animated.View
+          entering={FadeIn.delay(100)}
+          layout={LinearTransition}
+          style={{ alignItems: "center", justifyContent: "center" }}
+        >
+          <TouchableOpacity
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 100,
+              padding: 8,
+            }}
+            onPress={() => {
+              const today = new Date();
+              updateCurrentDate(today);
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 12,
+                color: colors.text,
+                fontWeight: "600",
+              }}
+            >
+              Heute
+            </Text>
+          </TouchableOpacity>
+        </Animated.View>
+      )}
       <TouchableOpacity
         style={{
           width: 32,
