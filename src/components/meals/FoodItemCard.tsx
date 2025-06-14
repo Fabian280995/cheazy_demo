622334d1cd45f2fe53c11de75225f3a85816cfaa
Fabian280995 <lessmannwebdev@gmail.com>
2025-06-14 17:31:45 +0200

    feat: integrate CategoryIcon into FoodItemCard for enhanced visual representation

--- src/components/meals/FoodItemCard.tsx
+++ src/components/meals/FoodItemCard.tsx
@@ -1,6 +1,7 @@
 import { FoodItem } from "@/types";
 import React from "react";
 import { Text, View } from "react-native";
+import CategoryIcon from "../shared/icons/CategoryIcon";
 
 export const FoodItemCard = ({ item }: { item: FoodItem }) => {
   const {
@@ -10,6 +11,7 @@ export const FoodItemCard = ({ item }: { item: FoodItem }) => {
     carbohydrates_per_100,
     protein_per_100,
     quantity,
+    category,
   } = item;
   const calories = Math.round((calories_per_100 * quantity) / 100);
 
@@ -18,22 +20,25 @@ export const FoodItemCard = ({ item }: { item: FoodItem }) => {
   const protein = Math.round((protein_per_100 * quantity) / 100);
 
   return (
-    <View>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-          gap: 8,
-        }}
-      >
-        <Text style={{ fontWeight: "bold" }}>{name}</Text>
-        <Text style={{ color: "gray" }}>{calories} kcal</Text>
+    <View style={{ flexDirection: "row", alignItems: "center", gap: 8 }}>
+      <CategoryIcon id={category} size={40} colorfull />
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            gap: 8,
+          }}
+        >
+          <Text style={{ fontWeight: "bold" }}>{name}</Text>
+          <Text style={{ color: "gray" }}>{calories} kcal</Text>
+        </View>
+        <Text style={{ fontSize: 12, color: "gray" }}>
+          {fat}g Fett, {carbohydrates}g Kohlenhydrate, {protein}g Eiweiß
+        </Text>
+        <Text style={{ fontSize: 12, color: "gray" }}>Menge: {quantity}g</Text>
       </View>
-      <Text style={{ fontSize: 12, color: "gray" }}>
-        {fat}g Fett, {carbohydrates}g Kohlenhydrate, {protein}g Eiweiß
-      </Text>
-      <Text style={{ fontSize: 12, color: "gray" }}>Menge: {quantity}g</Text>
     </View>
   );
 };
