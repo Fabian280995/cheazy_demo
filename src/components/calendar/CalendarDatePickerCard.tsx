b64456f6e228ba5b0399ab5d7a4ae9be448712fe
Fabian280995 <lessmannwebdev@gmail.com>
2025-06-14 19:17:32 +0200

    feat: enhance CalendarDatePickerCard with month navigation buttons and improved theme integration

--- src/components/calendar/CalendarDatePickerCard.tsx
+++ src/components/calendar/CalendarDatePickerCard.tsx
@@ -5,10 +5,12 @@ import Card from "../shared/Card";
 import CardHeader from "../shared/CardHeader";
 import CardIcon from "../shared/CardIcon";
 import DateScrollPicker from "./DateScrollPicker";
+import { TouchableOpacity, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 const CalendarDatePickerCard = () => {
-  const { categoryColors } = useTheme();
-  const { currentDate } = useCalendar();
+  const { categoryColors, colors } = useTheme();
+  const { currentDate, increaseMonth, decreaseMonth } = useCalendar();
   return (
     <Card>
       <CardHeader
@@ -24,7 +26,46 @@ const CalendarDatePickerCard = () => {
             gradient
           />
         )}
-      />
+      >
+        <View style={{ flexDirection: "row", gap: 8 }}>
+          <TouchableOpacity
+            style={{
+              width: 32,
+              height: 32,
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 100,
+              backgroundColor: colors.background,
+            }}
+            onPress={decreaseMonth}
+          >
+            <Feather
+              name="chevron-left"
+              size={24}
+              color={colors.icon}
+              style={{ marginLeft: -2 }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              width: 32,
+              height: 32,
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 100,
+              backgroundColor: colors.background,
+            }}
+            onPress={increaseMonth}
+          >
+            <Feather
+              name="chevron-right"
+              size={24}
+              color={colors.icon}
+              style={{ marginLeft: 3 }}
+            />
+          </TouchableOpacity>
+        </View>
+      </CardHeader>
 
       <DateScrollPicker />
     </Card>
