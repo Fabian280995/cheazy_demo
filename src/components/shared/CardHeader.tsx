d73e96580723c3e41a5385e87dc7c075c6c5b35d
Fabian280995 <lessmannwebdev@gmail.com>
2025-06-14 14:15:50 +0200

    feat: enhance CardHeader component to support children and improve layout structure

--- src/components/shared/CardHeader.tsx
+++ src/components/shared/CardHeader.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from "@/providers/theme";
-import React, { ReactNode } from "react";
+import React, { PropsWithChildren, ReactNode } from "react";
 import { Text, View } from "react-native";
 
 interface Props {
@@ -7,22 +7,31 @@ interface Props {
   title: string;
 }
 
-const CardHeader = ({ Icon, title }: Props) => {
+const CardHeader = ({ Icon, title, children }: PropsWithChildren<Props>) => {
   const { colors } = useTheme();
   return (
-    <View style={{ flexDirection: "row", alignItems: "center" }}>
-      {Icon && <Icon />}
-      <Text
-        style={{
-          fontFamily: "Nunito",
-          color: colors.text,
-          fontWeight: "800",
-          marginLeft: 8,
-          fontSize: 16,
-        }}
-      >
-        {title}
-      </Text>
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        {Icon && <Icon />}
+        <Text
+          style={{
+            fontFamily: "Nunito",
+            color: colors.text,
+            fontWeight: "800",
+            marginLeft: 8,
+            fontSize: 16,
+          }}
+        >
+          {title}
+        </Text>
+      </View>
+      {children}
     </View>
   );
 };
