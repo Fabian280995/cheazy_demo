4fcdcf2db2641ce04fba15a1e6448e63608be4b6
Fabian280995 <lessmannwebdev@gmail.com>
2025-06-16 18:24:30 +0200

    feat: implement useLocalSearchParams in FoodDetail component to retrieve food ID

--- src/app/(protected)/foods/[id].tsx
+++ src/app/(protected)/foods/[id].tsx
@@ -1,7 +1,9 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { useLocalSearchParams } from "expo-router";
 
 const FoodDetail = () => {
+  const { id } = useLocalSearchParams<{ id: string }>();
   return (
     <View>
       <Text>FoodDetail</Text>
