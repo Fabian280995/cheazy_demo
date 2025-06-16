8e5e22ecc962b3c04a2630e7c7ce47502d0cc5fd
Fabian280995 <lessmannwebdev@gmail.com>
2025-06-16 20:44:55 +0200

    refactor: streamline FoodDetailScreen layout and enhance visual structure with improved card components

--- src/screens/FoodDetailScreen.tsx
+++ src/screens/FoodDetailScreen.tsx
@@ -24,188 +24,183 @@ const FoodDetailScreen = ({
   const datetime = new Date();
   const mealSlot = MEAL_SLOTS.find((slot) => slot.id === "Breakfast");
   const quantity = 120;
-  const category = foodCategories.find(
-    (cat) => cat.id === (food.category_id as FoodCategoryId)
-  );
   return (
     <View style={{ flex: 1, padding: 16, gap: 16, position: "relative" }}>
-      <View style={{}}>
+      <Card
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 8,
+          marginBottom: 12,
+        }}
+      >
+        <CategoryIcon
+          id={(food.category_id as FoodCategoryId) ?? null}
+          size={48}
+          colorfull
+        />
+        <Text
+          style={{ fontWeight: "bold", fontSize: 32, fontFamily: "Nunito" }}
+        >
+          {food.name}
+        </Text>
+      </Card>
+      <Card style={{ marginBottom: 16 }}>
         <View
           style={{
             flexDirection: "row",
             alignItems: "center",
+            justifyContent: "space-evenly",
             gap: 8,
-            marginBottom: 12,
           }}
         >
-          <CategoryIcon
-            id={(food.category_id as FoodCategoryId) ?? null}
-            size={48}
-            colorfull
-          />
-          <Text
-            style={{ fontWeight: "bold", fontSize: 32, fontFamily: "Nunito" }}
+          <View
+            style={{
+              flex: 1,
+              borderRadius: 16,
+              borderWidth: 1,
+              borderColor: colors.border,
+              alignItems: "center",
+              justifyContent: "center",
+              height: 64,
+            }}
           >
-            {food.name}
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: "bold",
+                fontFamily: "Nunito",
+              }}
+            >
+              {datetime.toLocaleDateString("de-DE", {
+                weekday: "long",
+              })}
+            </Text>
+            <Text style={{ fontSize: 12, color: colors.textLight }}>
+              {datetime.toLocaleDateString("de-DE", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+              })}
+            </Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              borderRadius: 16,
+              borderWidth: 1,
+              borderColor: colors.border,
+              alignItems: "center",
+              justifyContent: "center",
+              height: 64,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: "bold",
+                fontFamily: "Nunito",
+              }}
+            >
+              {mealSlot ? mealSlot.label : "Frühstück"}
+            </Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              borderRadius: 16,
+              borderWidth: 1,
+              borderColor: colors.border,
+              alignItems: "center",
+              justifyContent: "center",
+              height: 64,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: "bold",
+                fontFamily: "Nunito",
+              }}
+            >
+              {quantity} g
+            </Text>
+          </View>
+        </View>
+        <TouchableOpacity
+          onPress={() => onAddFood(food)}
+          style={{
+            padding: 4,
+            borderRadius: 16,
+            flexDirection: "row",
+            alignItems: "center",
+            alignSelf: "flex-end",
+            marginTop: 12,
+          }}
+        >
+          <Feather name="plus" size={14} color={colors.primary} />
+          <Text
+            style={{
+              fontFamily: "Nunito",
+              fontSize: 12,
+              color: colors.primary,
+              marginLeft: 8,
+            }}
+          >
+            {addLabel}
+          </Text>
+        </TouchableOpacity>
+      </Card>
+      <Card>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: "Nunito",
+              fontSize: 16,
+              fontWeight: "800",
+            }}
+          >
+            Nährwerte
+          </Text>
+          <Text
+            style={{
+              fontFamily: "Inter",
+              fontSize: 16,
+              fontWeight: "700",
+              color: colors.secondary,
+              marginLeft: 8,
+            }}
+          >
+            {food.kcal_per_100} kcal
           </Text>
         </View>
-        <Card style={{ marginBottom: 16 }}>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-evenly",
-              gap: 8,
-            }}
-          >
-            <View
-              style={{
-                flex: 1,
-                borderRadius: 16,
-                borderWidth: 1,
-                borderColor: colors.border,
-                alignItems: "center",
-                justifyContent: "center",
-                height: 64,
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontWeight: "bold",
-                  fontFamily: "Nunito",
-                }}
-              >
-                {datetime.toLocaleDateString("de-DE", {
-                  weekday: "long",
-                })}
-              </Text>
-              <Text style={{ fontSize: 12, color: colors.textLight }}>
-                {datetime.toLocaleDateString("de-DE", {
-                  year: "numeric",
-                  month: "2-digit",
-                  day: "2-digit",
-                })}
-              </Text>
-            </View>
-            <View
-              style={{
-                flex: 1,
-                borderRadius: 16,
-                borderWidth: 1,
-                borderColor: colors.border,
-                alignItems: "center",
-                justifyContent: "center",
-                height: 64,
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontWeight: "bold",
-                  fontFamily: "Nunito",
-                }}
-              >
-                {mealSlot ? mealSlot.label : "Frühstück"}
-              </Text>
-            </View>
-            <View
-              style={{
-                flex: 1,
-                borderRadius: 16,
-                borderWidth: 1,
-                borderColor: colors.border,
-                alignItems: "center",
-                justifyContent: "center",
-                height: 64,
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontWeight: "bold",
-                  fontFamily: "Nunito",
-                }}
-              >
-                {quantity} g
-              </Text>
-            </View>
-          </View>
-          <TouchableOpacity
-            onPress={() => onAddFood(food)}
-            style={{
-              padding: 4,
-              borderRadius: 16,
-              flexDirection: "row",
-              alignItems: "center",
-              alignSelf: "flex-end",
-              marginTop: 12,
-            }}
-          >
-            <Feather name="plus" size={14} color={colors.primary} />
-            <Text
-              style={{
-                fontFamily: "Nunito",
-                fontSize: 12,
-                color: colors.primary,
-                marginLeft: 8,
-              }}
-            >
-              {addLabel}
-            </Text>
-          </TouchableOpacity>
-        </Card>
-        <Card>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text
-              style={{
-                fontFamily: "Nunito",
-                fontSize: 16,
-                fontWeight: "800",
-              }}
-            >
-              Nährwerte
-            </Text>
-            <Text
-              style={{
-                fontFamily: "Inter",
-                fontSize: 16,
-                fontWeight: "700",
-                color: colors.secondary,
-                marginLeft: 8,
-              }}
-            >
-              {food.kcal_per_100} kcal
-            </Text>
-          </View>
-          <View style={{ marginTop: 12, gap: 12 }}>
-            <NutritionBar
-              name="Kohlenhydrate"
-              value={food.carbs_g_per_100}
-              target={100}
-              categoryColorProfile="carbs"
-            />
-            <NutritionBar
-              name="Fette"
-              value={food.fat_g_per_100}
-              target={100}
-              categoryColorProfile="fat"
-            />
-            <NutritionBar
-              name="Proteine"
-              value={food.protein_g_per_100}
-              target={100}
-              categoryColorProfile="protein"
-            />
-          </View>
-        </Card>
-      </View>
+        <View style={{ marginTop: 12, gap: 12 }}>
+          <NutritionBar
+            name="Kohlenhydrate"
+            value={food.carbs_g_per_100}
+            target={100}
+            categoryColorProfile="carbs"
+          />
+          <NutritionBar
+            name="Fette"
+            value={food.fat_g_per_100}
+            target={100}
+            categoryColorProfile="fat"
+          />
+          <NutritionBar
+            name="Proteine"
+            value={food.protein_g_per_100}
+            target={100}
+            categoryColorProfile="protein"
+          />
+        </View>
+      </Card>
     </View>
   );
 };
