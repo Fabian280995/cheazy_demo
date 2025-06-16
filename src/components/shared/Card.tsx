6ef7e1c53cd564b3f90b4462c77a519a7f1a22d2
Fabian280995 <lessmannwebdev@gmail.com>
2025-06-16 20:00:16 +0200

    feat: add style prop to Card component for customizable styling

--- src/components/shared/Card.tsx
+++ src/components/shared/Card.tsx
@@ -1,18 +1,27 @@
 // components/DailyCaloriesCard.tsx
 import { useTheme } from "@/providers/theme";
 import React, { ReactNode } from "react";
-import { View } from "react-native";
+import { View, ViewStyle } from "react-native";
 
-const Card = ({ children }: { children: ReactNode }) => {
+const Card = ({
+  children,
+  style,
+}: {
+  children: ReactNode;
+  style?: ViewStyle;
+}) => {
   const { colors } = useTheme();
 
   return (
     <View
-      style={{
-        padding: 12,
-        borderRadius: 16,
-        backgroundColor: colors.foreground,
-      }}
+      style={[
+        {
+          padding: 12,
+          borderRadius: 16,
+          backgroundColor: colors.foreground,
+        },
+        style,
+      ]}
     >
       {children}
     </View>
