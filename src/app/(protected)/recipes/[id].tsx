76a5762445e1d009c046ddf88a6fc4b074fcb039
Fabian280995 <lessmannwebdev@gmail.com>
2025-06-19 19:39:19 +0200

    refactor: enhance RecipeDetail component with header options and navigation functionality

--- src/app/(protected)/recipes/[id].tsx
+++ src/app/(protected)/recipes/[id].tsx
@@ -1,11 +1,32 @@
-import { View, Text } from "react-native";
+import HeaderIconButton from "@/components/screens/HeaderIconButton";
+import { useHeaderOptions } from "@/hooks/navigation/useHeaderOptions";
+import { useTheme } from "@/providers/theme";
+import { Stack, useRouter } from "expo-router";
 import React from "react";
 
 const RecipeDetail = () => {
+  const router = useRouter();
+  const { colors } = useTheme();
+  const headerOptions = useHeaderOptions({
+    title: "",
+    largeTitle: false,
+  });
   return (
-    <View>
-      <Text>RecipeDetail</Text>
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          ...headerOptions,
+          headerLeft: () => (
+            <HeaderIconButton
+              iconName="arrow-left"
+              onPress={() => router.back()}
+              color={colors.text}
+              shadow
+            />
+          ),
+        }}
+      />
+    </>
   );
 };
 
