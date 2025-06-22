106e7f9e9c0dbabae0a874a0b28818651a032cff
Fabian280995 <lessmannwebdev@gmail.com>
2025-06-22 09:56:22 +0200

    feat: add updateRecipe function for updating existing recipes

--- src/api/recipes.ts
+++ src/api/recipes.ts
@@ -1,5 +1,10 @@
 import { supabase } from "@/lib/supabase";
-import { FoodItem, Recipe, RecipeModelForCreate } from "@/types";
+import {
+  FoodItem,
+  Recipe,
+  RecipeModelForCreate,
+  RecipeModelForUpdate,
+} from "@/types";
 
 export const createRecipe = async (
   newRecipe: RecipeModelForCreate
@@ -12,6 +17,42 @@ export const createRecipe = async (
   if (error) throw error;
   if (!data) throw new Error("Recipe creation failed");
 
+  return {
+    id: data.id,
+    name: data.name,
+    description: data.description || "",
+    servings: data.servings,
+    ingredients: data.recipe_ingredients
+      ? data.recipe_ingredients.map(
+          (rc) =>
+            ({
+              id: rc.food_id,
+              name: rc.food.name,
+              description: rc.food.description || "",
+              category: rc.food.category_id,
+              calories_per_100: rc.food.kcal_per_100,
+              protein_per_100: rc.food.protein_g_per_100,
+              carbohydrates_per_100: rc.food.carbs_g_per_100,
+              fat_per_100: rc.food.fat_g_per_100,
+              quantity: rc.quantity_g,
+            } as FoodItem)
+        )
+      : [],
+  };
+};
+
+export const updateRecipe = async (
+  recipeId: string,
+  updatedRecipe: RecipeModelForUpdate
+): Promise<Recipe> => {
+  const { data, error } = await supabase
+    .from("recipes")
+    .update(updatedRecipe)
+    .eq("id", recipeId)
+    .select("*, recipe_ingredients: recipe_ingredients(*, food: food_id(*))")
+    .single();
+  if (error) throw error;
+  if (!data) throw new Error("Recipe update failed");
   return {
     id: data.id,
     name: data.name,
--- src/types/recipes.ts
+++ src/types/recipes.ts
@@ -3,6 +3,7 @@ import { Tables } from "./supabase";
 
 export type RecipeModel = Tables<"recipes">;
 export type RecipeModelForCreate = Omit<RecipeModel, "id">;
+export type RecipeModelForUpdate = Partial<RecipeModel>;
 
 export type RecipeIngredient = Tables<"recipe_ingredients">;
 export type RecipeIngredientForCreate = RecipeIngredient;
