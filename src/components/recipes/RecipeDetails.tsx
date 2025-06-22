48d8576f929e4ff85bb77f35ca9fc1f24fdc06d7
Fabian280995 <lessmannwebdev@gmail.com>
2025-06-22 13:33:06 +0200

    feat: integrate RecipeDetails component with servings management and nutrition overview in RecipeDetailScreen

--- src/components/recipes/RecipeDetails.tsx
+++ src/components/recipes/RecipeDetails.tsx
@@ -1,146 +1,76 @@
 import { useTheme } from "@/providers/theme";
-import { NutritionTotals, Recipe } from "@/types";
-import { Feather } from "@expo/vector-icons";
-import { useRouter } from "expo-router";
+import { Recipe } from "@/types";
 import React from "react";
-import { useWindowDimensions, View } from "react-native";
-import Animated, { LinearTransition } from "react-native-reanimated";
-import { FoodItemCard } from "../food/FoodItemCard";
-import AddEntryButton from "../meal-slots/AddEntryButton";
+import { TextInput, View } from "react-native";
 import { QuantitySelect } from "../meal-slots/QuantitySelect";
-import { NutritionOverview } from "../nutrition/NutritionOverview";
-import DetailScreenHeader from "../screens/DetailScreenHeader";
-import DetailScreenScroll from "../screens/DetailScreenScroll";
 import CardHeader from "../shared/CardHeader";
-import { SwipeableListItem } from "../shared/list/ListItem";
-import { useDeleteRecipeIngredient } from "@/hooks/recipe-ingredients/useDeleteRecipeIngredient";
 
 interface Props {
   recipe: Recipe;
+  servings: number;
+  onServingsChange: (newServings: number) => void;
 }
 
-const RecipeDetails = ({ recipe }: Props) => {
+const RecipeDetails = ({ recipe, servings, onServingsChange }: Props) => {
   const { colors } = useTheme();
-  const router = useRouter();
-  const height = useWindowDimensions().height;
-  const { ingredients } = recipe;
+  const [title, setTitle] = React.useState<string>(recipe.name);
+  const [description, setDescription] = React.useState<string>(
+    recipe.description ?? ""
+  );
 
-  const { mutateAsync: removeIngredient, isPending: isRemoving } =
-    useDeleteRecipeIngredient();
-
-  const [portions, setPortions] = React.useState<number>(recipe.servings);
-  const quantity = React.useMemo(() => {
-    return ingredients.reduce((acc, item) => acc + item.quantity, 0);
-  }, [ingredients]);
-  const totalQuantity = quantity / portions;
-
-  const totals: NutritionTotals = React.useMemo(() => {
-    return ingredients.reduce(
-      (acc, item) => {
-        acc.calories += item.calories_per_100 * (item.quantity / 100);
-        acc.carbs += item.carbohydrates_per_100 * (item.quantity / 100);
-        acc.fat += item.fat_per_100 * (item.quantity / 100);
-        acc.protein += item.protein_per_100 * (item.quantity / 100);
-        return acc;
-      },
-      { calories: 0, carbs: 0, fat: 0, protein: 0 }
-    );
-  }, [ingredients, quantity, portions]);
-
-  const handleAddIngredient = () => {
-    router.push(`/recipes/${recipe.id}/foods`);
-  };
-
-  const handleDeleteIngredient = async (foodId: string) => {
-    await removeIngredient({
-      recipeId: recipe.id,
-      foodId,
-    });
-  };
-
-  const handleIngredientPress = (foodId: string) => {
-    router.push(`/recipes/${recipe.id}/foods/${foodId}`);
-  };
+  React.useEffect(() => {
+    setTitle(recipe.name);
+    setDescription(recipe.description ?? "");
+  }, [recipe]);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: colors.background,
-        position: "relative",
-      }}
-    >
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: colors.background,
-          paddingTop: 16,
-        }}
-      >
-        {true && (
-          <View
+    <>
+      <View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <TextInput
             style={{
-              width: "100%",
-              height: height / 4 + 32,
-              alignItems: "center",
-              justifyContent: "center",
+              fontWeight: "800",
+              fontSize: 24,
+              fontFamily: "Nunito",
+              maxWidth: "80%",
             }}
-          >
-            <Feather name="camera" size={64} color={colors.textLight} />
-          </View>
-        )}
-      </View>
-      <DetailScreenScroll>
-        <DetailScreenHeader
-          title={recipe.name}
-          description={recipe.description}
-        />
-
-        <View style={{}}>
-          <CardHeader title="Portionen" size={20} />
-          <QuantitySelect
-            quantity={portions}
-            onChangeQuantity={setPortions}
-            step={1}
+            value={title}
+            onChangeText={setTitle}
+            placeholder="Rezeptname"
           />
         </View>
 
-        <NutritionOverview
-          title={"Nährwerte pro Portion"}
-          calories={totals.calories / portions}
-          carbs={totals.carbs / portions}
-          fat={totals.fat / portions}
-          protein={totals.protein / portions}
-          target={totalQuantity}
-        />
-
-        <Animated.View layout={LinearTransition} key="ingredients">
-          <CardHeader title="Zutaten" size={20} />
-          <AddEntryButton
-            id={"add-button"}
-            onPress={handleAddIngredient}
+        <View style={{ gap: 4 }}>
+          <TextInput
+            multiline
+            value={description}
+            onChangeText={setDescription}
+            placeholder="Keine Beschreibung verfügbar. Füge eine hinzu."
             style={{
-              paddingHorizontal: 0,
-              borderBottomWidth: 0,
+              fontFamily: "Nunito",
+              fontSize: 16,
+              fontWeight: "500",
+              color: colors.textLight,
             }}
-            isFirst
           />
-          {ingredients.map((item) => {
-            return (
-              <SwipeableListItem
-                isLast={item === ingredients[ingredients.length - 1]}
-                key={item.id}
-                onPress={() => handleIngredientPress(item.id)}
-                onDelete={() => handleDeleteIngredient(item.id)}
-                style={{ paddingHorizontal: 0 }}
-              >
-                <FoodItemCard item={item} />
-              </SwipeableListItem>
-            );
-          })}
-        </Animated.View>
-      </DetailScreenScroll>
-    </View>
+        </View>
+      </View>
+
+      <View>
+        <CardHeader title="Portionen" size={20} />
+        <QuantitySelect
+          quantity={servings}
+          onChangeQuantity={onServingsChange}
+          step={1}
+        />
+      </View>
+    </>
   );
 };
 
--- src/screens/RecipeDetailScreen.tsx
+++ src/screens/RecipeDetailScreen.tsx
@@ -1,10 +1,140 @@
+import { FoodItemCard } from "@/components/food/FoodItemCard";
+import AddEntryButton from "@/components/meal-slots/AddEntryButton";
+import { NutritionOverview } from "@/components/nutrition/NutritionOverview";
 import RecipeDetails from "@/components/recipes/RecipeDetails";
+import DetailScreenScroll from "@/components/screens/DetailScreenScroll";
+import CardHeader from "@/components/shared/CardHeader";
+import { SwipeableListItem } from "@/components/shared/list/ListItem";
+import { useDeleteRecipeIngredient } from "@/hooks/recipe-ingredients/useDeleteRecipeIngredient";
 import { useRecipe } from "@/providers/recipe";
-import React from "react";
+import { useTheme } from "@/providers/theme";
+import { NutritionTotals } from "@/types";
+import { Feather } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import React, { useEffect } from "react";
+import { useWindowDimensions, View } from "react-native";
+import Animated, { LinearTransition } from "react-native-reanimated";
 
 const RecipeDetailScreen = () => {
   const { recipe } = useRecipe();
-  return <RecipeDetails recipe={recipe} />;
+  const { colors } = useTheme();
+  const router = useRouter();
+  const height = useWindowDimensions().height;
+  const { ingredients } = recipe;
+  const [servings, setServings] = React.useState<number>(recipe.servings);
+
+  const { mutateAsync: removeIngredient, isPending: isRemoving } =
+    useDeleteRecipeIngredient();
+
+  const quantity = React.useMemo(() => {
+    return ingredients.reduce((acc, item) => acc + item.quantity, 0);
+  }, [ingredients]);
+  const totalQuantity = quantity / servings;
+
+  const totals: NutritionTotals = React.useMemo(() => {
+    return ingredients.reduce(
+      (acc, item) => {
+        acc.calories += item.calories_per_100 * (item.quantity / 100);
+        acc.carbs += item.carbohydrates_per_100 * (item.quantity / 100);
+        acc.fat += item.fat_per_100 * (item.quantity / 100);
+        acc.protein += item.protein_per_100 * (item.quantity / 100);
+        return acc;
+      },
+      { calories: 0, carbs: 0, fat: 0, protein: 0 }
+    );
+  }, [ingredients, quantity, servings]);
+
+  const handleAddIngredient = () => {
+    router.push(`/recipes/${recipe.id}/foods`);
+  };
+
+  const handleDeleteIngredient = async (foodId: string) => {
+    await removeIngredient({
+      recipeId: recipe.id,
+      foodId,
+    });
+  };
+
+  const handleIngredientPress = (foodId: string) => {
+    router.push(`/recipes/${recipe.id}/foods/${foodId}`);
+  };
+
+  useEffect(() => {
+    if (recipe) {
+      setServings(recipe.servings);
+    }
+  }, [recipe]);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: colors.background,
+        position: "relative",
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: colors.background,
+          paddingTop: 16,
+        }}
+      >
+        {true && (
+          <View
+            style={{
+              width: "100%",
+              height: height / 4 + 32,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Feather name="camera" size={64} color={colors.textLight} />
+          </View>
+        )}
+      </View>
+      <DetailScreenScroll>
+        <RecipeDetails
+          recipe={recipe}
+          servings={servings}
+          onServingsChange={setServings}
+        />
+        <NutritionOverview
+          title={"Nährwerte pro Portion"}
+          calories={totals.calories / servings}
+          carbs={totals.carbs / servings}
+          fat={totals.fat / servings}
+          protein={totals.protein / servings}
+          target={totalQuantity}
+        />
+        <Animated.View layout={LinearTransition} key="ingredients">
+          <CardHeader title="Zutaten" size={20} />
+          <AddEntryButton
+            id={"add-button"}
+            onPress={handleAddIngredient}
+            style={{
+              paddingHorizontal: 0,
+              borderBottomWidth: 0,
+            }}
+            isFirst
+          />
+          {ingredients.map((item) => {
+            return (
+              <SwipeableListItem
+                isLast={item === ingredients[ingredients.length - 1]}
+                key={item.id}
+                onPress={() => handleIngredientPress(item.id)}
+                onDelete={() => handleDeleteIngredient(item.id)}
+                style={{ paddingHorizontal: 0 }}
+              >
+                <FoodItemCard item={item} />
+              </SwipeableListItem>
+            );
+          })}
+        </Animated.View>
+      </DetailScreenScroll>
+    </View>
+  );
 };
 
 export default RecipeDetailScreen;
