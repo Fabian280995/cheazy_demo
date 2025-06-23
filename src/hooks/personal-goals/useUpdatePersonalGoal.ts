069332a9ef54f9afdfe96555d662cbb5cf4b1ffe
Fabian280995 <lessmannwebdev@gmail.com>
2025-06-23 10:02:41 +0200

    refactor: remove unused import and clean up code in useUpdatePersonalGoal hook

--- src/hooks/personal-goals/useUpdatePersonalGoal.ts
+++ src/hooks/personal-goals/useUpdatePersonalGoal.ts
@@ -1,13 +1,11 @@
-import { createPersonalGoal, updatePersonalGoal } from "@/api/personal-goals";
+import { updatePersonalGoal } from "@/api/personal-goals";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useRouter } from "expo-router";
 
 export const useUpdatePersonalGoal = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationKey: ["personalGoals", "update"],
-    // caller passes { id, updates }
     mutationFn: updatePersonalGoal,
     onSuccess: (_, { id }) => {
       queryClient.invalidateQueries({ queryKey: ["personalGoals", id] });
